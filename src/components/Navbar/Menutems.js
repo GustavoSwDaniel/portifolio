870751244767d556200a7a2d32ce65627ad33a36
Gustavo Daniel <gustavodanieldetoledo@gmail.com>
2021-08-17 11:33:05 -0300

    WIP

---
+++ src/components/Navbar/Menutems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: "Projetos",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Sobre",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Curriculo",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Contanto",
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
